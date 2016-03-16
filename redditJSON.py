84489a4e15279a254ad295fa775ee69fdffa696b
apd01 <alandunne85@gmail.com>
2016-03-16 18:55:28 -0400

    Temporary fix replies issue

--- redditJSON.py
+++ redditJSON.py
@@ -11,7 +11,7 @@ r.config.store_json_result = True
 
 
 # Get submission
-submissions = r.get_subreddit('bjj').get_top_from_hour()
+submissions = r.get_subreddit('mma').get_top_from_hour()
 submission = next(submissions)
 submission = next(submissions)
 submission = next(submissions)
@@ -56,6 +56,7 @@ with open(filename + '.txt', 'a') as outfile:
         output += '\n' # outfile.write('\n')
         #outfile.write(json.dumps(c, indent=4, sort_keys=True))
         print('\n' + output + '\n')
+        # This doesn't work if there are any replies
         output += json.dumps(c.json_dict, indent=4, sort_keys=True, ensure_ascii=True) #outfile.write(json.dumps(c.json_dict, indent=4, sort_keys=True, ensure_ascii=True))
 
         output += ',\n'# outfile.write(',\n')
